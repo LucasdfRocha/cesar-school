83334a82b3db3d0f8a5fa56924b3c2421b4d3feb
LucasdfRocha <ldfr@cesar.school>
2022-09-03 12:16:17 -0300

    working on inicio.c

--- 2-Periodo/Algoritimo-e-estrutura-de-dados/Aula02/inicio.c
+++ 2-Periodo/Algoritimo-e-estrutura-de-dados/Aula02/inicio.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-/**********************************************************
+/****************************************************************************************
 01. Considere cada operação:
 a) x > 3 && x >= 10 || (! (x > 5)).
 Quando x = 7, qual será a saída? 
@@ -19,20 +19,14 @@ resolução:
 (2 == 7 &&  9 > 3) || 9 < 9 && 9 + ++2 < 3
             (0 && 1) || 0 && 1
             0 || 0
-*********************************************************/
-int main(void) {
-
-/******************************
- 
+--------------------------------------------------
 02. Faça um programa em C que leia dois números inteiros (A e B), realize e exiba o
 resultado dos seguintes cálculos.
 a) A parte inteira da divisão entre os valores de A e B
 b) O resto da divisão entre os valores de A e B
 c) 35% do valor de B
-
-
-
-********************************/
+****************************************************************************************/
+int main(void) {
 
   int A,B,resultado,resto;
   float porcentagem;
@@ -48,10 +42,5 @@ c) 35% do valor de B
 
   printf("Divisao inteira igual a %d",resultado);
   printf("\nresto igual a %d ",resto);
-  printf("\n35 porcento de B resulta em %f porcento",porcentagem);
-  
-
-  
-  
-  
+  printf("\n35 porcento de B resulta em %f porcento",porcentagem); 
 }
